cf86b38cb82016a1fcd9e5e65f3744196d48c5f1
timurrumit0 <timur.chapliuk@gmail.com>
2023-03-04 09:53:58 +0200

    first commit

---
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.clicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textViewResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickClicker(View view){
+        findViewById(R.id.textView);
+
+    }
+}
